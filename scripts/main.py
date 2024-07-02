8b1a36ca48ef9d4858b80cfdd04270d02f394de7
morixxfoxdata <morixx.datascience@gmail.com>
2024-07-02 14:12:19 +0900

    main

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,11 @@
 from src.normal_method.data import hadamard_total
-from src.normal_method.speckle import inv_hadamard, speckle_noise_calculation
+from src.normal_method.speckle import inv_hadamard
 from src.normal_method.visualization.display import display_image
 
 hadamard_sp = inv_hadamard()
-predicted_speckle = speckle_noise_calculation(hadamard_sp)
+# predicted_speckle = speckle_noise_calculation(hadamard_sp)
 X_hadamard, y_hadamard = hadamard_total()
-
-display_image(2, X_hadamard, predicted_speckle)
+print(hadamard_sp.shape)
+print(X_hadamard.shape)
+print(y_hadamard.shape)
+display_image(4, hadamard_sp, X_hadamard, y_hadamard)
